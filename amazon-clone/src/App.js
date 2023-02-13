2510bf19b4026ce5a5fb4cf9347d3026c7e47cc3
dplong1993 <dplong@crimson.ua.edu>
2023-02-13 15:19:54 -0600

    Day One of following walkthrough. Built front page

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+// import SearchIcon from '@mui/icons-material/Search';
+
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDlc3xSat8ndRIZRP1LbirWG39WO9L68a0",
+  authDomain: "clone-v1-5d0c0.firebaseapp.com",
+  projectId: "clone-v1-5d0c0",
+  storageBucket: "clone-v1-5d0c0.appspot.com",
+  messagingSenderId: "1087846133655",
+  appId: "1:1087846133655:web:23d4b86768c8073e5f5ed2",
+  measurementId: "G-Y720RG7SP5"
+};
