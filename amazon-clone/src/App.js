e895cc85277a33f42005adb9f8eab570bac04e97
dplong1993 <dplong@crimson.ua.edu>
2023-02-13 16:18:45 -0600

    Added code for Checkout Component. Also added links to header component to allow for easy navigation between checkout and home page

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   return (
     <Router>
       <div className="app">
+        <Header />
         <Routes>
-          <Route path="/" element={[<Header />, <Home />]} />
-          <Route path="/checkout" element={<h1>THIS IS CHECKOUT!!!!</h1>} />
+          <Route path="/" element={[<Home />]} />
+          <Route path="/checkout" element={[<Checkout />]} />
         </Routes>
       </div>
     </Router>
